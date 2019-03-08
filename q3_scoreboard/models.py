9b7d319cd8813f95b31f7ceb0725bc9bc1d5c8a8
Erik Parreira <etinlb@gmail.com>
2019-03-08 16:54:09 -0500

    Fleshing out the models a bit

--- q3_scoreboard/models.py
+++ q3_scoreboard/models.py
@@ -3,12 +3,43 @@ from . import db
 
 def init_db():
     db.create_all()
+    # add the WORLD Player
 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __init__(self, username):
+        self.username = username
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+    @staticmethod
+    def get_user_or_create_user(username):
+        user = User.query.filter_by(username=username).first()
+        if user is None:
+            user = User(username)
+            db.session.add(user)
+            db.session.commit()
+        return user
+
+
+class Game(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    mapname = db.Column(db.String(80))
+
+
+class GameKill(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    game_id = db.Column(db.Integer, db.ForeignKey('game.id'))
+    killer = db.Column(db.Integer, db.ForeignKey('user.id'))
+    victum = db.Column(db.Integer, db.ForeignKey('user.id'))
+    method = db.Column(db.String(80))
+
+    def __init__(self, game_id, killer, victum, method):
+        self.game_id = game_id
+        self.killer = killer
+        self.victum = victum
+        self.method = method
