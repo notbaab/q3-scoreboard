28a3f5eb914c6a2df46572274c2f01535497ee49
Erik Parreira <etinlb@gmail.com>
2020-03-22 17:28:08 -0700

    Split the data loader into the the streamer and file loader

--- q3_scoreboard/game_loader.py
+++ q3_scoreboard/game_loader.py
@@ -1,6 +1,9 @@
+"""
+Functions responsible for loading games from full server log files
+"""
 from . import models
 from . import db
-from quake3_log_parser import parser
+from quake3_log_parser import lineparsers, tracker
 
 
 def get_user_id_from_username(username):
@@ -12,7 +15,33 @@ def get_user_id_from_username(username):
 def get_userid(player):
     if player.is_bot:
         return models.BOT_ID
-    return get_user_id_from_username(player.name)
+    return models.User.get_user_id(player.name)
+
+
+def load_game(game):
+    print("adding game")
+    game_model = models.Game()
+    db.session.add(game_model)
+    # commit so we can get the game id
+    db.session.commit()
+    for stat_table in game.stat_table.values():
+        # TODO: What to do about bots....Probably handle in the
+        # load_stat_table_killl_entry
+        load_kill_stat_table(game_model.id, stat_table)
+        load_scores(game_model.id, stat_table)
+
+    # load the disconnected players stat tables if they aren't bots
+    for stat_table in game.disconnected_player_stat_table:
+        if stat_table.player.is_bot:
+            continue
+        load_kill_stat_table(game_model.id, stat_table)
+
+
+def load_from_text(input_data):
+    games = lineparsers.parse_str(input_data)
+
+    for game in games:
+        load_game(game)
 
 
 # Do everything based on death so we have suicides added
@@ -38,29 +67,3 @@ def load_kill_stat_table(game_id, stat_table):
     # print(stat_table)
     for kills in stat_table.kill_by_player.values():
         load_stat_table_kill_entry(game_id, kills, False)
-
-
-def add_game(game):
-    print("adding game")
-    game_model = models.Game()
-    db.session.add(game_model)
-    # commit so we can get the game id
-    db.session.commit()
-    for stat_table in game.stat_table.values():
-        # TODO: What to do about bots....Probably handle in the
-        # load_stat_table_killl_entry
-        load_kill_stat_table(game_model.id, stat_table)
-        load_scores(game_model.id, stat_table)
-
-    # load the disconnected players stat tables if they aren't bots
-    for stat_table in game.disconnected_player_stat_table:
-        if stat_table.player.is_bot:
-            continue
-        load_kill_stat_table(game_model.id, stat_table)
-
-
-def load_from_text(input_data):
-    games = parser.parse_str(input_data)
-
-    for game in games:
-        add_game(game)
---
+++ q3_scoreboard/game_streamer.py
@@ -0,0 +1,99 @@
+from . import models
+from . import db
+from quake3_log_parser import lineparsers, tracker, datastructures
+
+
+def get_user_id_from_username(username):
+    user = models.User.get_user_or_create_user(username)
+    return user.id
+
+
+# returns the username or <BOT> of the player
+def get_userid(player):
+    if player.is_bot:
+        return models.BOT_ID
+    return get_user_id_from_username(player.name)
+
+
+def add_kill(game_id, killer_name, victum_name, method):
+    # print(stat_table)
+    killer_id = get_user_id_from_username(killer_name)
+    victum_id = get_user_id_from_username(victum_name)
+    kill_entry = models.GameKill(game_id, killer_id, victum_id, method)
+    db.session.add(kill_entry)
+
+
+def add_final_score(player_id, score, game_id):
+    score = models.Score(player_id, score, game_id)
+    db.session.add(score)
+    db.session.commit()
+
+
+def read_game_from_queue(game_start_data, queue, stop_flag):
+    """Continually read from the queue and stream out the updates to the db
+    """
+    game_model = models.Game()
+    game_model.mapname = game_start_data["mapname"]
+    db.session.add(game_model)
+
+    # commit so we can get the game id
+    db.session.commit()
+    game_id = game_model.id
+
+    # need the game tracker cause it keeps the mapping of player id to
+    # username. I'm not happy about this, it should be gooder
+    game_tracker = datastructures.Game(game_model.mapname)
+
+    while not stop_flag.stop:
+        line = queue.get()
+        line_type = lineparsers.get_line_type(line)
+
+        if line_type is None:
+            continue
+
+        # parse it for the tracker
+        if line_type == lineparsers.LineType.GAME_SHUTDOWN:
+            print("game is done")
+            game_tracker.print_summary()
+            return
+
+        tracker.track_functions[line_type](game_tracker, line)
+
+        if line_type == lineparsers.LineType.PLAYER_INFO:
+            player = lineparsers.parse_player_added(line)
+            if not player.is_bot:
+                # TODO: Don't rely on this to add a new user to the database
+                get_userid(player)
+        elif line_type == lineparsers.LineType.KILL:
+            killer_name, victum_name, method = lineparsers.parse_kill(line)
+            add_kill(game_id, killer_name, victum_name, method)
+        elif line_type == lineparsers.LineType.SCORE:
+            player_id_in_game, score = lineparsers.parse_final_score(line)
+            # the final score parser only returns the local game id, use it
+            # to get the player name. Even though the final score line has the
+            # playername in it
+            player_tracker = game_tracker.players.get(player_id_in_game, None)
+            if player_tracker is None:
+                print("something fucky happened, couldn't find %s" % player_name)
+                continue
+            print("adding player score for %s " % player_tracker.name)
+            player_database_id = get_userid(player_tracker)
+            add_final_score(player_database_id, score, game_id)
+
+
+def read_from_queue(queue, stop_flag):
+    """Continually read from the queue and stream out the updates to the db
+    """
+    print("Loading from queue")
+    game = None
+    # no close method on the queue so just gotta wait until we get the stop
+    # signal
+    while not stop_flag.stop:
+        # grab the game
+        line = queue.get()
+        line_type = lineparsers.get_line_type(line)
+        if line_type == lineparsers.LineType.INIT_GAME:
+            print("Game Started")
+            game_data = lineparsers.parse_start_game(line)
+            print(game_data)
+            read_game_from_queue(game_data, queue, stop_flag)
