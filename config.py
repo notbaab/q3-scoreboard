79e1fba3c6ae3fd417618c9ff47ca89425d4beb6
Erik Parreira <etinlb@gmail.com>
2020-03-22 17:25:19 -0700

    Config variables for the local mac development

--- config.py
+++ config.py
@@ -1,9 +1,19 @@
 import os
+from pathlib import Path
+
 dir_path = os.path.dirname(os.path.realpath(__file__))
+home = str(Path.home())
 
 # Enable Flask's debugging features. Should be False in production
 DEBUG = True
 PREFERRED_URL_SCHEME = 'http'
+HOST = "0.0.0.0"
+SAVE_RAW_UPLOADED_FILES = True
+
+IOQUAKE_PATCH_DIR = os.path.join(dir_path, "data", "mac", "baseq3")
+IOQUAKE_BASEQ3_DIR = os.path.join(home, "Library/Application Support/Quake3/baseq3")
+IOQUAKE_SERVER_EXE = ("/Applications/ioquake3/ioquake3.app/Contents/MacOS/"
+                      "ioq3ded")
 
 DATABASE_FILE = "data.db"
 SQLALCHEMY_DATABASE_URI = "sqlite:////{}/{}".format(dir_path, DATABASE_FILE)
