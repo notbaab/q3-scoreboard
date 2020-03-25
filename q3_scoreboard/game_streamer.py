ad2fc58ac1af4a41888d4a393e0eec93d521afb1
Erik Parreira <etinlb@gmail.com>
2020-03-25 12:28:20 -0700

    Fixed a bug in the streamer code
    Not using the correct id

--- q3_scoreboard/game_streamer.py
+++ q3_scoreboard/game_streamer.py
@@ -8,18 +8,21 @@ def get_user_id_from_username(username):
     return user.id
 
 
-# returns the username or <BOT> of the player
+# returns the username of the player or BOT if a bot
 def get_userid(player):
     if player.is_bot:
         return models.BOT_ID
     return get_user_id_from_username(player.name)
 
 
-def add_kill(game_id, killer_name, victum_name, method):
-    # print(stat_table)
-    killer_id = get_user_id_from_username(killer_name)
-    victum_id = get_user_id_from_username(victum_name)
-    kill_entry = models.GameKill(game_id, killer_id, victum_id, method)
+def handle_kill_line(game_tracker, game_db_id, line):
+    killer_game_id, victum_game_id, method = lineparsers.parse_kill(line)
+    killer = game_tracker.players.get(killer_game_id, None)
+    victum = game_tracker.players.get(victum_game_id, None)
+
+    killer_id = get_userid(killer)
+    victum_id = get_userid(victum)
+    kill_entry = models.GameKill(game_db_id, killer_id, victum_id, method)
     db.session.add(kill_entry)
 
 
@@ -65,8 +68,7 @@ def read_game_from_queue(game_start_data, queue, stop_flag):
                 # TODO: Don't rely on this to add a new user to the database
                 get_userid(player)
         elif line_type == lineparsers.LineType.KILL:
-            killer_name, victum_name, method = lineparsers.parse_kill(line)
-            add_kill(game_id, killer_name, victum_name, method)
+            handle_kill_line(game_tracker, game_id, line)
         elif line_type == lineparsers.LineType.SCORE:
             player_id_in_game, score = lineparsers.parse_final_score(line)
             # the final score parser only returns the local game id, use it
@@ -74,7 +76,8 @@ def read_game_from_queue(game_start_data, queue, stop_flag):
             # playername in it
             player_tracker = game_tracker.players.get(player_id_in_game, None)
             if player_tracker is None:
-                print("something fucky happened, couldn't find %s" % player_name)
+                print("something fucky happened, couldn't find %s" %
+                      player_tracker.name)
                 continue
             print("adding player score for %s " % player_tracker.name)
             player_database_id = get_userid(player_tracker)
@@ -85,7 +88,6 @@ def read_from_queue(queue, stop_flag):
     """Continually read from the queue and stream out the updates to the db
     """
     print("Loading from queue")
-    game = None
     # no close method on the queue so just gotta wait until we get the stop
     # signal
     while not stop_flag.stop:
